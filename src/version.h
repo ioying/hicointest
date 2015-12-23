0a5c4228a14abe6ef60c0c0722f0f64f27c2a845
Coin Developer <coindev@vultr.guest>
2015-12-24 06:41:49 +0900

    Version Change

--- src/version.h
+++ src/version.h
@@ -13,7 +13,7 @@
 
 static const int CLIENT_VERSION =
                            1000000 * CLIENT_VERSION_MAJOR
-                         +   00000 * CLIENT_VERSION_MINOR
+                         +   10000 * CLIENT_VERSION_MINOR
                          +     100 * CLIENT_VERSION_REVISION
                          +       1 * CLIENT_VERSION_BUILD;
 
